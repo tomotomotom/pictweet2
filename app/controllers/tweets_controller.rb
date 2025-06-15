87674be9c74c96bbba9e758a51b2fb751d4a9fb7
tomotomotom <torimorinorikeyhome@gmail.com>
2025-06-15 14:15:28 +0900

    投稿者本人のみに編集・削除を許可まで

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,16 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_index, except: [:index, :show]
+
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user)
   end
 
   def new
     @tweet = Tweet.new
   end
 
-    def create
+  def create
     Tweet.create(tweet_params)
     redirect_to '/'
   end
@@ -18,9 +21,31 @@ class TweetsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
+
+  def show
+  end
+
   private
   def tweet_params
-    params.require(:tweet).permit(:name, :image, :text)
+    params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
