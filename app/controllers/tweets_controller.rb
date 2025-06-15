b5913f2795e9b49793fc9180727be8618cc9daf7
tomotomotom <torimorinorikeyhome@gmail.com>
2025-06-15 11:04:01 +0900

    tweet編集まで

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,26 @@
+class TweetsController < ApplicationController
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+    def create
+    Tweet.create(tweet_params)
+    redirect_to '/'
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to root_path
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
+
+end
